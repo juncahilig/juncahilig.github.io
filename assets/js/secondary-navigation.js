adcb795ddfcc97929e16670083ffbc11eaf989a2
Jun <juncahilig08@gmail.com>
2016-04-21 14:54:08 +0800

    initial commit

---
+++ assets/js/secondary-navigation.js
@@ -0,0 +1,24 @@
+(function ($) {
+	$(document).ready(function(){
+
+		// TODO: Optimize Selector
+		// TODO: Optimize Selector
+		// TODO: Beautify Tranisition
+
+	    $(function () {
+	    	$(window).scroll(function () {
+
+				var topElement = $('#who').offset().top;
+				var scrollTop = $(this).scrollTop();
+
+				// set distance user needs to scroll before we start fadeIn
+				if ( scrollTop > topElement - 145) {
+					$('.secondary-navigation').fadeIn();
+				} else {
+					$('.secondary-navigation').fadeOut();
+				}
+
+			});
+		});
+	});
+}(jQuery));
